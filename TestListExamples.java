a8023d580d7711e68549bdf8c621c206ab46eba7
dwang507 <davidwang334@gmail.com>
2022-11-15 22:17:29 -0800

    temp commit

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,54 @@
 import static org.junit.Assert.*;
+
+
 import org.junit.*;
 
+import java.util.List;
+import java.util.ArrayList;
+
+
+class Checker implements StringChecker {
+
+  @Override
+  public boolean checkString(String s) {
+    if (s.length() > 3) {
+      return true;
+    }
+    return false;
+  }
+  
+}
+
+
 public class TestListExamples {
-  // Write your grading tests here!
+    @Test
+    public void filterTest() {
+        List<String> input = new ArrayList<>();
+        input.add("pass");
+        input.add("no");
+        input.add("sdf");
+        input.add("pass2");
+        Checker check = new Checker();
+        List<String> output = new ArrayList<>();
+        output.add("pass");
+        output.add("pass2");
+
+        assertEquals(output, ListExamples.filter(input, check));
+    }
+
+    @Test
+    public void mergeTest() {
+        List<String> input1 = new ArrayList<>();
+        input1.add("a");
+        input1.add("b");
+        List<String> input2 = new ArrayList<>();
+        input2.add("c");
+        input2.add("d");
+        List<String> output = new ArrayList<>();
+        output.add("a");
+        output.add("b");
+        output.add("c");
+        output.add("d");
+        assertEquals(output, ListExamples.merge(input1, input2));
+    }
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,36 @@
 # Create your grading script here
 
-set -e
+CP=".:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar"
 
 rm -rf student-submission
 git clone $1 student-submission
+
+cp TestListExamples.java student-submission
+
+cd student-submission
+
+if [ -e ListExamples.java ]
+then
+    javac -cp $CP *.java
+
+    echo "Compiled"
+    java -cp $CP org.junit.runner.JUnitCore TestListExamples > output.txt
+    if [ $? -eq 0 ]
+    then
+        echo "100%"
+        exit
+    fi
+    grep "failure" output.txt > result.txt
+    exit
+fi
+echo "File was not found!"
+    exit
+
+
+
+
+
+
+
+
 
